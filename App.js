765862b423d1db0060174d2e43602303f28f62a5
Daniel <dbenjovitz@gmail.com>
2023-10-02 14:31:53 +0200

    first

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
+import { useState } from 'react';
 
 export default function App() {
+  const [markers, setMarkers] = useState([])
+  const [region, setRegion] = useState({
+    latitude: 62,
+    longitude: -7,
+    latitudeDelta: 2,
+    longitudeDelta: 2
+  })
+
+  function addMarker(data){
+    const {latitude, longitude} = data.nativeEvent.coordinate
+    const newMarker = {
+      coordinate: {latitude, longitude},
+      key: data.timeStamp,
+      title: "Great place"
+    }
+
+    setMarkers([...markers, newMarker])
+  }
+
+  function onMarkerPressedText(text){
+    alert(`you pressed ${text}`)
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <MapView style={styles.map}
+       region={region}
+       onLongPress={addMarker}>
+        {markers.map(marker => (
+        <Marker coordinate={marker.coordinate}
+        key={marker.key}
+        title={marker.title}
+        onPress={() => onMarkerPressedText(marker.title)}
+        />
+       ))}
+       </MapView>
+       
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  map: {
+    width: "100%",
+    height: "100%"
   },
 });
